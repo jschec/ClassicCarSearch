58ceff6509c298374e527fbe5b4d6d77993755b4
jschec <scheck.joshua@gmail.com>
2023-04-22 18:32:07 -0700

    Added layout component

--- front_end/src/app/shared/layout/layout.component.ts
+++ front_end/src/app/shared/layout/layout.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class LayoutComponent {
 
+  private _mobileQueryListener: () => void;
+  mobileQuery: MediaQueryList;
+  showSpinner: boolean = false;
+  userName: string = "";
+  isAdmin: boolean = false;
+
+  private autoLogoutSubscription: Subscription = new Subscription;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef,
+      private media: MediaMatcher,
+      public spinnerService: SpinnerService,
+      private authService: AuthenticationService,
+      private authGuard: AuthGuard) {
+
+      this.mobileQuery = this.media.matchMedia('(max-width: 1000px)');
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      // tslint:disable-next-line: deprecation
+      this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    const user = this.authService.getCurrentUser();
+
+    this.isAdmin = user.isAdmin;
+    this.userName = user.fullName;
+
+    // Auto log-out subscription
+    const timer$ = timer(2000, 5000);
+    this.autoLogoutSubscription = timer$.subscribe(() => {
+        this.authGuard.canActivate();
+    });
+  }
+
+  ngOnDestroy(): void {
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+    this.autoLogoutSubscription.unsubscribe();
+}
+
+  ngAfterViewInit(): void {
+      this.changeDetectorRef.detectChanges();
+  }
 }

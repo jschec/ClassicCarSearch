a243776e9e336e8cdbafb7ed33bde43028a1b0eb
jschec <scheck.joshua@gmail.com>
2023-04-22 18:32:27 -0700

    Added app routing module

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # README
+
+## Development
+
+### Front End
+The front end for this project is being developed with Angular. To contribute to the front end development efforts, please install the required Angular dependencies mentioned in the following documentation: https://angular.io/guide/setup-local
+
+To install the dependencies for the 
+`npm install`
+
+Running the development server
+`ng serve --open`
--- front_end/src/app/app-routing.module.ts
+++ front_end/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

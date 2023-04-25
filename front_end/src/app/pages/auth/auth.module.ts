6e7c5b64824364a19e70767314b3f9c6640d1244
jschec <scheck.joshua@gmail.com>
2023-04-24 20:16:16 -0700

    Replaced auth module with login module

--- front_end/src/app/pages/auth/auth.module.ts
+++ front_end/src/app/pages/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth/auth.component';
-
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    LoginComponent
   ],
   imports: [
     CommonModule

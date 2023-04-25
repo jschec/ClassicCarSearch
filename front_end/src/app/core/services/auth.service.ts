6ab724a8189096890d8920bb9690cb5f2a9587e0
jschec <scheck.joshua@gmail.com>
2023-04-24 23:25:06 -0700

    Added getCurrentUser method to auth.service.ts

--- front_end/src/app/core/services/auth.service.ts
+++ front_end/src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SocialUser } from '@abacritt/angularx-social-login';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,15 @@ export class AuthService {
     private http: HttpClient,
     @Inject('LOCALSTORAGE') private localStorage: Storage
   ) {}
+
+  login(user: SocialUser) {
+    this.localStorage.setItem('currentUser', JSON.stringify(user));
+  }
+
+  getCurrentUser(): any {
+    // TODO: Enable after implementation
+    return JSON.parse(this.localStorage.getItem('currentUser') as any);
+}
 
   logout(): void {
     // clear token remove user from local storage to log user out

8e16853e421a07eadcaa50055cff75c8ddc2513a
jschec <scheck.joshua@gmail.com>
2023-04-24 17:39:23 -0700

    Added subscription page module

---
+++ front_end/src/app/pages/subscription/subscription.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubscriptionComponent } from './subscription/subscription.component';
+
+
+
+@NgModule({
+  declarations: [
+    SubscriptionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubscriptionModule { }

3f8d6cf41bbf8b6fc4ecc7d638abe5f3fbb795cf
jschec <scheck.joshua@gmail.com>
2023-04-24 21:31:22 -0700

    Added recommendation component

---
+++ front_end/src/app/pages/watch-list/watch-list-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LayoutComponent } from 'src/app/shared/layout/layout.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
+import { WatchListComponent } from './watch-list/watch-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: WatchListComponent },
+      { path: '/recommendation/:id', component: RecommendationComponent },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class WatchListRoutingModule { }
--- front_end/src/app/pages/watch-list/watch-list.module.ts
+++ front_end/src/app/pages/watch-list/watch-list.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { WatchListRoutingModule } from './watch-list-routing.module';
 import { WatchListComponent } from './watch-list/watch-list.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RecommendationComponent } from './recommendation/recommendation.component';
 
 @NgModule({
   declarations: [
-    WatchListComponent
+    WatchListComponent,
+    RecommendationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    WatchListRoutingModule,
   ]
 })
 export class WatchListModule { }

fd9b9229c93c4fb456e72cf04c3fa5c9c550bb2d
jschec <scheck.joshua@gmail.com>
2023-04-24 20:28:53 -0700

    Configured routing for landing module

--- front_end/src/app/pages/landing/landing.module.ts
+++ front_end/src/app/pages/landing/landing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing/landing.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     LandingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    LandingRoutingModule
   ]
 })
 export class LandingModule { }

7092d2e1f82d86c621860660a959f642d693b80e
jschec <scheck.joshua@gmail.com>
2023-04-24 20:31:28 -0700

    Added routing to privacy module

--- front_end/src/app/pages/privacy/privacy.module.ts
+++ front_end/src/app/pages/privacy/privacy.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { PrivacyRoutingModule } from './privacy-routing.module';
 import { PrivacyComponent } from './privacy/privacy.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     PrivacyComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    PrivacyRoutingModule
   ]
 })
 export class PrivacyModule { }

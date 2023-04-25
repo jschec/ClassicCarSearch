67e63a4b71624b6d8a0f02290ac7bb0763f0835b
jschec <scheck.joshua@gmail.com>
2023-04-24 20:25:12 -0700

    Added routing to about module

--- front_end/src/app/pages/about/about.module.ts
+++ front_end/src/app/pages/about/about.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     AboutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    AboutRoutingModule
   ]
 })
 export class AboutModule { }

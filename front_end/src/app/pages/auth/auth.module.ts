a9f64f24836b42f972cb780166dc1e414cab3eb4
jschec <scheck.joshua@gmail.com>
2023-04-24 17:36:05 -0700

    Added auth page module

---
+++ front_end/src/app/pages/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth/auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }

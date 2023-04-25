9d9dc9f45df8f17f1131dee91778901df0c7ffe3
jschec <scheck.joshua@gmail.com>
2023-04-24 17:40:11 -0700

    Added watch-list page module

---
+++ front_end/src/app/pages/watch-list/watch-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WatchListComponent } from './watch-list/watch-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    WatchListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WatchListModule { }

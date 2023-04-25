2b72c3d17ec46d23d45612a15b60031c76469dcb
jschec <scheck.joshua@gmail.com>
2023-04-24 20:19:26 -0700

    Updated auth module config

--- front_end/src/app/pages/auth/auth.module.ts
+++ front_end/src/app/pages/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }

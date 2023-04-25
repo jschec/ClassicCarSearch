7c2dcb1f1a8924fd72736057e6ced4d0a67e250e
jschec <scheck.joshua@gmail.com>
2023-04-24 23:25:17 -0700

    Added auth guard

--- front_end/src/app/core/guards/auth.guard.ts
+++ front_end/src/app/core/guards/auth.guard.ts
@@ -1,15 +1,44 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { 
+  ActivatedRouteSnapshot, 
+  CanActivate, 
+  Router, 
+  RouterStateSnapshot, 
+  UrlTree 
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
+
+import { AuthService } from '../services/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    const user = this.authService.getCurrentUser();
+
+    if (user) {
+      if (moment() < moment(user.expiration)) {
+        return true;
+      } else {
+        //this.notificationService.openSnackBar('Your session has expired');
+        this.router.navigate(['auth/login']);
+        return false;
+      }
+
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
   }
   
 }

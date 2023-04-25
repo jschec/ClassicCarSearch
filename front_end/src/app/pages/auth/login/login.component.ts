259298d50b58347b46863c8b2cbc85727790f6ad
jschec <scheck.joshua@gmail.com>
2023-04-24 23:24:50 -0700

    Updated login component controller

--- front_end/src/app/pages/auth/login/login.component.ts
+++ front_end/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { 
+  GoogleLoginProvider, 
+  SocialAuthService, 
+  SocialUser 
+} from '@abacritt/angularx-social-login';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +13,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  GoogleLoginProvider = GoogleLoginProvider;
+  loading: boolean = false;
 
+  constructor(
+    private router: Router,
+    private readonly socialAuthService: SocialAuthService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.socialAuthService.authState.subscribe(
+      user => {
+        this.authService.login(user);
+        this.router.navigate(['/']);
+      },
+      error => {
+        // TODO - somehow notify the user
+        this.loading = false;
+      }
+    );
+  }
 }

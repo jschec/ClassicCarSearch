3e13189116a2f09a0398647773255e2be8445c0c
jschec <scheck.joshua@gmail.com>
2023-04-25 17:48:11 -0700

    Added material imports for shared.module

--- front_end/src/app/shared/shared.module.ts
+++ front_end/src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { ThemeModule } from '../theme/theme.module';
 import { LayoutComponent } from './layout/layout.component';
-
 
 @NgModule({
   imports: [
     RouterModule,
-    ThemeModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // Material imports
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   declarations: [
     LayoutComponent

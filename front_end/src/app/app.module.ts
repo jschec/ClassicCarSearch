72957ada37ad2cc07ed7a399966dd2a4bf3e3920
jschec <scheck.joshua@gmail.com>
2023-04-25 17:48:42 -0700

    Removed references to theme module in app.module.ts

--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -9,7 +9,6 @@ import {
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { ThemeModule } from './theme/theme.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -22,7 +21,6 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     CoreModule,
     SharedModule,
-    ThemeModule.forRoot(),
     SocialLoginModule,
     AppRoutingModule,
   ],

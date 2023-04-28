7879ad4694b4f899ff46d55fd789930afba386e7
jschec <scheck.joshua@gmail.com>
2023-04-27 18:39:42 -0700

    Added router module index

---
+++ rest_api/app/routes/index.ts
@@ -0,0 +1,34 @@
+import express, { Router } from 'express';
+import listingRoutes from './listing';
+import searchRoutes from './search';
+import watchListRoutes from './watch-list';
+
+const router = express.Router();
+
+interface IRoute {
+  prefix: string;
+  routes: Router;
+}
+
+// Definition of route collections
+const routeCollections: IRoute[] = [
+  {
+    prefix: "/listings",
+    routes: listingRoutes
+  },
+  {
+    prefix: "/searches",
+    routes: searchRoutes
+  },
+  {
+    prefix: "/watch-lists",
+    routes: watchListRoutes
+  }
+];
+
+// Add collections of routes to the express application router
+routeCollections.forEach((routeCollection) => {
+  router.use(routeCollection.prefix, routeCollection.routes);
+});
+
+export default router;

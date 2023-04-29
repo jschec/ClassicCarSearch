705d5c50a99cd296723fc83356b04bf2f73142da
jschec <scheck.joshua@gmail.com>
2023-04-29 15:02:50 -0700

    Fixed id reference in car-listing.service.ts

--- rest_api/src/services/car-listing.service.ts
+++ rest_api/src/services/car-listing.service.ts
@@ -20,24 +20,24 @@ export const create = async (reqBody: NewCarListingBody): Promise<ICarListingDoc
 /**
  * Retrieves the specified CarListing record
  * 
- * @param {Types.ObjectId} id The identifier of the CarListing to retrieve
+ * @param {Types.ObjectId} carListingId The identifier of the CarListing to retrieve
  * @returns {Promise<ICarListingDoc | null>} A promise containing the specified CarListing record
  */
-export const getById = async (id: Types.ObjectId): Promise<ICarListingDoc | null> => {
-  return CarListing.findById(id);
+export const getById = async (carListingId: Types.ObjectId): Promise<ICarListingDoc | null> => {
+  return CarListing.findById(carListingId);
 };
 
 /**
  * Updates the CarListing record with the sought identifier.
  * 
- * @param {Types.ObjectId} carId The identifier of the CarListing to update
+ * @param {Types.ObjectId} carListingId The identifier of the CarListing to update
  * @param {UpdateCarListingBody} reqBody The request body supplied by the client
  * @returns {Promise<ICarListingDoc | null>} A promise containing the updated CarListing record
  */
 export const updateById = async (
-  carId: Types.ObjectId, reqBody: UpdateCarListingBody
+  carListingId: Types.ObjectId, reqBody: UpdateCarListingBody
 ): Promise<ICarListingDoc | null> => {
-  const record = await getById(carId);
+  const record = await getById(carListingId);
   
   if (!record) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Car listing not found');
@@ -53,11 +53,11 @@ export const updateById = async (
 /**
  * Deletes the CarListing record with the sought identifier.
  * 
- * @param {Types.ObjectId} carId The identifier of the CarListing to update
+ * @param {Types.ObjectId} carListingId The identifier of the CarListing to update
  * @returns {Promise<ICarListingDoc | null>} A promise containing the deleted CarListing record.
  */
-export const deleteById = async (carId: Types.ObjectId): Promise<ICarListingDoc | null> => {
-  const record = await getById(carId);
+export const deleteById = async (carListingId: Types.ObjectId): Promise<ICarListingDoc | null> => {
+  const record = await getById(carListingId);
   
   if (!record) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Car listing not found');

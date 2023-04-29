986b3d88710a096fc1e32c00661f4eed25847744
jschec <scheck.joshua@gmail.com>
2023-04-29 10:59:56 -0700

    Fixed id references for user and subscription routes

--- rest_api/src/routes/subscription.ts
+++ rest_api/src/routes/subscription.ts
@@ -10,8 +10,14 @@ import {
 const router = Router();
 
 router.post("/", (req, res, next) => createSubscription(req, res, next));
-router.get("/:id", (req, res, next) => getSubscription(req, res, next));
-router.put("/:id", (req, res, next) => updateSubscription(req, res, next));
-router.delete("/:id", (req, res, next) => deleteSubscription(req, res, next));
+router.get(
+  "/:subscriptionId", (req, res, next) => getSubscription(req, res, next)
+);
+router.put(
+  "/:subscriptionId", (req, res, next) => updateSubscription(req, res, next)
+);
+router.delete(
+  "/:subscriptionId", (req, res, next) => deleteSubscription(req, res, next)
+);
 
 export default router;

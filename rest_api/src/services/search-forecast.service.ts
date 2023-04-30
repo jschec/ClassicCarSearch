4195c17ed3d52abe2676e91a2f019947682c3dbe
jschec <scheck.joshua@gmail.com>
2023-04-30 07:31:40 -0700

    Added search-forecast service to service module

---
+++ rest_api/src/services/search-forecast.service.ts
@@ -0,0 +1,70 @@
+import httpStatus from 'http-status';
+import { Types } from 'mongoose';
+
+import { 
+  NewSearchForecastBody, UpdateSearchForecastBody, ISearchForecastDoc 
+} from '../interfaces/search-forecast.interfaces';
+import SearchForecast from '../models/search-forecast.model';
+import ApiError from '../utils/ApiError';
+
+/**
+ * Creates a new SearchForecast record
+ * 
+ * @param {NewSearchForecastBody} reqBody The request body supplied by the client
+ * @returns {Promise<ISearchForecastDoc>} A promise containing the new SearchForecast record
+ */
+export const create = async (reqBody: NewSearchForecastBody): Promise<ISearchForecastDoc> => {
+  return SearchForecast.create(reqBody);
+};
+
+/**
+ * Retrieves the specified SearchForecast record
+ * 
+ * @param {Types.ObjectId} searchForecastId The identifier of the SearchForecast to retrieve
+ * @returns {Promise<ISearchForecastDoc | null>} A promise containing the specified SearchForecast record
+ */
+export const getById = async (searchForecastId: Types.ObjectId): Promise<ISearchForecastDoc | null> => {
+  return SearchForecast.findById(searchForecastId);
+};
+
+/**
+ * Updates the SearchForecast record with the sought identifier.
+ * 
+ * @param {Types.ObjectId} searchForecastId The identifier of the SearchForecast to update
+ * @param {UpdateSearchForecastBody} reqBody The request body supplied by the client
+ * @returns {Promise<ISearchForecastDoc | null>} A promise containing the updated SearchForecast record
+ */
+export const updateById = async (
+  searchForecastId: Types.ObjectId, reqBody: UpdateSearchForecastBody
+): Promise<ISearchForecastDoc | null> => {
+  const record = await getById(searchForecastId);
+  
+  if (!record) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Search forecast not found');
+  }
+  
+  Object.assign(record, reqBody);
+  
+  await record.save();
+  
+  return record;
+};
+
+/**
+ * Deletes the SearchForecast record with the sought identifier.
+ * 
+ * @param {Types.ObjectId} searchForecastId The identifier of the SearchForecast to update
+ * @returns {Promise<ISearchForecastDoc | null>} A promise containing the deleted SearchForecast record.
+ */
+export const deleteById = async (searchForecastId: Types.ObjectId): Promise<ISearchForecastDoc | null> => {
+  const record = await getById(searchForecastId);
+  
+  if (!record) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Search forecast not found');
+  }
+
+  // TODO - remove all associated records
+
+  await record.deleteOne();
+  return record;
+};

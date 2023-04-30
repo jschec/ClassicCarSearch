4465beb912b888987ef6d6c09ed4ec8470efe3cd
jschec <scheck.joshua@gmail.com>
2023-04-30 07:57:19 -0700

    Configured search endpoints

--- rest_api/src/routes/search.ts
+++ rest_api/src/routes/search.ts
@@ -1,25 +1,14 @@
 import { Router } from 'express';
 
+import {
+  createSearch, deleteSearch, getSearch, updateSearch, 
+} from '../controllers/search.controller';
+
 const router = Router();
 
-router.get("/", (req, res, next) => {
-
-});
-
-router.post("/", (req, res, next) => {
-
-});
-
-router.get("/:id", (req, res, next) => {
-
-});
-
-router.put("/:id", (req, res, next) => {
-
-});
-
-router.delete("/:id", (req, res, next) => {
-
-});
+router.post("/", (req, res, next) => createSearch(req, res, next));
+router.get("/:searchId", (req, res, next) => getSearch(req, res, next));
+router.put("/:searchId", (req, res, next) => updateSearch(req, res, next));
+router.delete("/:searchId", (req, res, next) => deleteSearch(req, res, next));
 
 export default router;

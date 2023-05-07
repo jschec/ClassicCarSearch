b09c16ec3f78591c1c5def3b8b03975136f11d4a
weston cox <coxw1@seattleu.edu>
2023-05-07 15:38:46 -0700

    listing component boilerplate

--- front_end/src/app/pages/search/listing.component.ts
+++ front_end/src/app/pages/search/listing.component.ts
@@ -3,10 +3,19 @@ import { FormControl } from '@angular/forms';
 import { ListingService, IListing } from 'src/app/core/services/listing.service';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  selector: 'app-listing',
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.scss']
 })
 export class ListingComponent {
-  
+  listings: IListing[] = [];
+  constructor(private listingService: ListingService) {}
+
+  ngOnInit(): void {
+    this.listingService.getRecords().subscribe((response) => {
+      this.listing = response;
+
+      console.log(this.listing);
+    });
+  }
 }

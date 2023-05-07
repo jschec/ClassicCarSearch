83adcd736df1c7746974dbc29c869485c5471d57
Zhihai Deng <dzhsurf@gmail.com>
2023-05-07 00:55:25 -0700

    Added WatchList basic layout and style.

--- front_end/src/app/pages/watch-list/watch-list.component.ts
+++ front_end/src/app/pages/watch-list/watch-list.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+// import { LayoutModule } from '@angular/material/layout';
+// import { MatCardModule } from '@angular/material/card';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-watch-list',
   templateUrl: './watch-list.component.html',
-  styleUrls: ['./watch-list.component.scss']
+  styleUrls: ['./watch-list.component.scss'],
 })
 export class WatchListComponent {
-
+  tiles: Tile[] = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
+  items = [
+    {
+      name: "1",
+    },
+    {
+      name: "2",
+    },
+    {
+      name: "3",
+    },
+    {
+      name: "4",
+    },
+  ];
+  longText = "alsdfjlaskd fjlsajflaskjfd asdfasdf asf asdf sdfasf asdf  asdf asdfasdf asd asdf asdf asdfasdf asdf asdfasdfasdf laskfjlasfdjlas lasdkfjlasdkjf lsadj flasdjfla sdfjlaskdfalskdfaaaaaa";
 }

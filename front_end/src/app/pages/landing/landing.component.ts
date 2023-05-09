fb781f255f51bc55734827e0a5b6ee680b5de071
jschec <scheck.joshua@gmail.com>
2023-05-08 19:38:40 -0700

    Added initial form group implementation

--- front_end/src/app/pages/landing/landing.component.ts
+++ front_end/src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
+  filterForm: FormGroup;
+  
+  regionOptions = [
+    "Northeast",
+    "Southwest",
+    "West",
+    "Southeast",
+    "Midwest"
+  ]
 
+  constructor() {
+    const minYear = 1885;
+    const maxYear = new Date().getFullYear() + 1;
+
+    this.filterForm = new FormGroup({
+      make: new FormControl(''),
+      model: new FormControl(''),
+      selectedRegion: new FormControl(''),
+      startYear: new FormControl(
+        minYear, [Validators.min(minYear), Validators.max(maxYear)]
+      ),
+      endYear: new FormControl(
+        maxYear, [Validators.min(minYear), Validators.max(maxYear)]
+      )
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClickSubmit() {
+    console.log(this.filterForm.value);
+  }
 }

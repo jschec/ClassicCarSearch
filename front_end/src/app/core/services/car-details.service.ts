973caa04898df87e5e769627f0cd955bd1613f17
AndresRodriguez82 <114323671+AndresRodriguez82@users.noreply.github.com>
2023-05-13 23:26:12 -0700

    added services for car-detail component

---
+++ front_end/src/app/core/services/car-details.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+export interface ICar {
+  make: string;
+  model: string;
+  year: number;
+  exteriorCondition: Condition;
+  mechanicalCondition: Condition;
+  mileage: number;
+  color: string;
+  img: string;
+}
+
+export interface ICarSeller {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export interface ICarListing {
+  region: string;
+  price: number;
+  listDate: Date;
+  saleDate: Date | null;
+  seller: ICarSeller; //this should be the seller id
+  car: ICar; // this should be the car id
+}
+export enum Condition {
+  Excellent = 'Excellent',
+  Good = 'Good',
+  Fair = 'Fair',
+  Bad = 'Bad',
+}
+
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarDetailsService {
+
+  constructor(private http: HttpClient) { }
+
+  public getBylistingId(carlistingId: string): Observable<ICarListing> {
+    const url = '/api/car-listings/' + carlistingId ;
+    return this.http.get<ICarListing>(url); 
+  }
+
+ 
+}

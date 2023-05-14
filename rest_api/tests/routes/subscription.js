34b52a02952e181637e50184479d72c8eae7bfb7
jschec <scheck.joshua@gmail.com>
2023-05-14 09:08:27 -0700

    Added type tests to subscription.js

--- rest_api/tests/routes/subscription.js
+++ rest_api/tests/routes/subscription.js
@@ -26,15 +26,20 @@ describe('Test get all subscriptions', function() {
     
   it('Should return an array of 4 objects', function () {
 		expect(response).to.have.status(200);
+    expect(response.body).to.be.an('array');
 		expect(response.body).to.have.length(4);
 		expect(response).to.have.headers;
   });
     
 	it('The first object in the array has known properties', function() {
     expect(requestResult[0]).to.have.property('id');
+    expect(requestResult[0].id).to.be.a('string');
+
     expect(requestResult[0]).to.have.property('name');
+    expect(requestResult[0].name).to.be.a('string');
+
     expect(requestResult[0]).to.have.property('cost');
-		expect(response.body).to.not.be.a.string;
+		expect(requestResult[0].cost).to.be.a('number');
 	});
 
 	it('The elements in the array have the expected properties', function() {
@@ -42,8 +47,13 @@ describe('Test get all subscriptions', function() {
 			function (body) {
 				for (var i = 0; i < body.length; i++) {
 					expect(body[i]).to.have.property('id');
+          expect(body[i].id).to.be.a('string');
+
 					expect(body[i]).to.have.property('name');
-					expect(body[i]).to.have.property('cost');
+					expect(body[i].name).to.be.a('string');
+
+          expect(body[i]).to.have.property('cost');
+          expect(body[i].cost).to.be.a('number');
 				}
 				return true;
 			});
@@ -76,8 +86,14 @@ describe('Test get single subscription', function() {
     
 	it('The object in the array has known properties', function() {
     expect(requestResult).to.have.property('id');
+    expect(requestResult.id).to.be.a('string');
+
     expect(requestResult).to.have.property('name');
+    expect(requestResult.name).to.be.a('string');
+
     expect(requestResult).to.have.property('cost');
-		expect(response.body).to.not.be.a.string;
+		expect(requestResult.cost).to.be.a('number');
+
+    expect(response.body).to.not.be.a.string;
 	});
 });

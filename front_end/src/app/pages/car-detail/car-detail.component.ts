e0817de5b02f929368d1b45dfdfb96ff5adb9f0c
AndresRodriguez82 <114323671+AndresRodriguez82@users.noreply.github.com>
2023-05-13 23:24:38 -0700

    added car-detail component

--- front_end/src/app/pages/car-detail/car-detail.component.ts
+++ front_end/src/app/pages/car-detail/car-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { of, Observable, concat } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { ICarListing,CarDetailsService} from 'src/app/core/services/car-details.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-detail',
@@ -6,5 +13,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./car-detail.component.scss']
 })
 export class CarDetailComponent {
+  // from backend
+  carListing: ICarListing | null = null;
+  loading: boolean = true;
+  id: string;
+
+  constructor(private route: ActivatedRoute,
+    private carDetailsService: CarDetailsService){
+    this.id = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+
+    this.carDetailsService.getBylistingId(this.id).subscribe((response) => {
+    
+    this.carListing = response;
+    
+    });
+    
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+  
 
 }

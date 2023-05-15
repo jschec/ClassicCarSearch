f3b1bc0dd970c41cac8a725fd89a4a8c24ac11c3
jschec <scheck.joshua@gmail.com>
2023-05-14 21:02:13 -0700

    Fixed formatting in search component

--- front_end/src/app/pages/search/search.component.ts
+++ front_end/src/app/pages/search/search.component.ts
@@ -7,86 +7,86 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
 })
 
 export class SearchComponent {
-  regionOptions = [
+  regionOptions = [
     "",
-    "Northeast",
-    "Southwest",
-    "West",
-    "Southeast",
-    "Midwest"
-  ]
+    "Northeast",
+    "Southwest",
+    "West",
+    "Southeast",
+    "Midwest"
+  ]
 
-  qualityOptions = [
+  qualityOptions = [
     "",
-    "Bad",
-    "Fair",
-    "Good",
-    "Excellent"
-  ]
+    "Bad",
+    "Fair",
+    "Good",
+    "Excellent"
+  ]
 
-  filterForm: FormGroup;  
-  searchResults: ICarListing[] = [];
-  page: number = 0;
-  pageSizeList: number[] = [5, 10, 25, 100]
-  pageSize: number = this.pageSizeList[0];
-  numRecords: number = 0;
+  filterForm: FormGroup;
+  searchResults: ICarListing[] = [];
+  page: number = 0;
+  pageSizeList: number[] = [5, 10, 25, 100]
+  pageSize: number = this.pageSizeList[0];
+  numRecords: number = 0;
 
-constructor(private route: ActivatedRoute, private router: Router, private searchService: SearchService) {
-const minYear = 1885;
-const maxYear = new Date().getFullYear() + 1;
+  constructor(private route: ActivatedRoute, private router: Router, private searchService: SearchService) {
+    const minYear = 1885;
+    const maxYear = new Date().getFullYear() + 1;
 
-this.filterForm = new FormGroup({
-make: new FormControl(''),
-model: new FormControl(''),
-region: new FormControl(''),
-startYear: new FormControl(
-minYear, [Validators.min(minYear), Validators.max(maxYear)]
-),
-endYear: new FormControl(
-maxYear, [Validators.min(minYear), Validators.max(maxYear)]
-),
-exteriorCondition: new FormControl(''),
-mechanicalCondition: new FormControl('')
-});
-}
+    this.filterForm = new FormGroup({
+      make: new FormControl(''),
+      model: new FormControl(''),
+      region: new FormControl(''),
+      startYear: new FormControl(
+        minYear, [Validators.min(minYear), Validators.max(maxYear)]
+      ),
+      endYear: new FormControl(
+        maxYear, [Validators.min(minYear), Validators.max(maxYear)]
+      ),
+      exteriorCondition: new FormControl(''),
+      mechanicalCondition: new FormControl('')
+    });
+  }
 
-applySearch(): void {
-this.searchService.getRecords(
-this.page, this.pageSize, this.filterForm.value
-).subscribe((response) => {
-this.searchResults = response.records;
-this.numRecords = response.numRecords;
-});
-}
+  applySearch(): void {
+    this.searchService.getRecords(
+      this.page, this.pageSize, this.filterForm.value
+    ).subscribe((response) => {
+      this.searchResults = response.records;
+      this.numRecords = response.numRecords;
+    });
+  }
 
-ngOnInit(): void {
-this.route.queryParams.subscribe(params => {
-      if (params["searchCriteria"]) {
-        const parsedParams = JSON.parse(params["searchCriteria"]);
-        this.filterForm.patchValue(parsedParams);
-}
-this.applySearch();
-    });
-}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params["searchCriteria"]) {
+        const parsedParams = JSON.parse(params["searchCriteria"]);
+        this.filterForm.patchValue(parsedParams);
+      }
+      this.applySearch();
+    });
+  }
 
-onClickSubmit() {
-this.applySearch();
-}
+  onClickSubmit() {
+    this.applySearch();
+  }
 
-onPageChanged(event: PageEvent): void {
-    this.page = event.pageIndex;
-    this.pageSize = event.pageSize;
-    this.applySearch();
-}
+  onPageChanged(event: PageEvent): void {
+    this.page = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.applySearch();
+  }
 
-goToDetail(carRecord: ICar){
-    this.router.navigate(['/car', carRecord._id])
-}
+  goToDetail(carRecord: ICar) {
+    this.router.navigate(['/car', carRecord._id])
+  }
 
 }

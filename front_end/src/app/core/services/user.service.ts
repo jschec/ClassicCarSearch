800fbec3dbe7534452371720e97a6d778c9318ce
jschec <scheck.joshua@gmail.com>
2023-05-16 22:11:46 -0700

    Updated user service implementation

--- front_end/src/app/core/services/user.service.ts
+++ front_end/src/app/core/services/user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  pictureUri: string;
+  age: number;
+  subscription: string;
+  watchList: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUser(userId: string): Observable<IUser> {
+    const url = `/api/user/${userId}`;
+    return this.http.get<IUser>(url);
+  }
 }

af0148c6140b85a89a705a7fa73a6feed196d8ba
weston cox <coxw1@seattleu.edu>
2023-05-24 20:49:39 -0700

    Draft code for chart in car detail

--- front_end/src/app/pages/car-detail/car-detail.component.ts
+++ front_end/src/app/pages/car-detail/car-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ICarListing,CarDetailsService, ISearchForecast} from 'src/app/services/car-details.service';
 import { ActivatedRoute } from '@angular/router';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-detail',
@@ -15,6 +16,7 @@ export class CarDetailComponent {
   loading: boolean = true;
   id: string;
   forecast: ISearchForecast | null = null;
+  chart: any;
 
   constructor(private route: ActivatedRoute,
     private carDetailsService: CarDetailsService){
@@ -28,7 +30,36 @@ export class CarDetailComponent {
 
     //Plot price history, if exists
     if (this.forecast  != null){
+      this.createChart();      
+    }
+  }
+
+  createChart(){
+    this.chart = new Chart("MyChart", {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: [],
+        datasets: [
+          {
+            label: "Average Sale Price",
+            data: [],
+            backgroundColor: 'blue'
+          },
+        ]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
       
+
+    });
+    if (this.forecast != null) {
+      let monthCount = this.forecast.priceHistory.length;
+      for (let j = 0; j < monthCount; j++){
+        this.chart.labels.push(j);
+        this.chart.datasets.data.push(this.forecast.priceHistory[j]);
+      }
     }
   }
 }

dc5c4357a5b8e6448fc1f1ced986f3a4a0660bd8
jschec <scheck.joshua@gmail.com>
2023-05-25 18:28:29 -0700

    Added logic for creating or identifying user

--- rest_api/src/utils/GoogleOAuth.ts
+++ rest_api/src/utils/GoogleOAuth.ts
@@ -2,6 +2,8 @@ import passport from 'passport';
 import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
 
 import config from '../config';
+import User from '../models/user.model';
+import { IUser } from '../interfaces/user.interfaces';
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -18,12 +20,21 @@ passport.use(new GoogleStrategy({
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
-    console.log("profile", profile);
+    let user: IUser | null = null;
+
+    User.findOne({ ssoID: profile.id }).then((user) => user);
+
+    if (!user) {
+      User.create({
+        ssoID: profile.id,
+        firstName: profile.name?.givenName,
+        lastName: profile.name?.familyName,
+        email: profile.emails?.[0].value,
+        pictureUri: profile.photos?.[0].value,
+        age: -1
+      }).then((user) => user);
+    }
 
     done(null, profile);
-    
-    //User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //  return done(err, user);
-    //});
   }
 ));

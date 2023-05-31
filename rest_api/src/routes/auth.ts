ffb1d56e9fc8f41badd0f773365bd677f0139510
jschec <scheck.joshua@gmail.com>
2023-05-30 19:13:14 -0700

    Added additional authentication endpoints

--- rest_api/src/routes/auth.ts
+++ rest_api/src/routes/auth.ts
@@ -2,24 +2,33 @@ import { Router } from 'express';
 import passport from 'passport';
 
 import config from '../config';
+import { IUser, ISafeUser } from '../interfaces/user.interfaces';
 
 const router = Router();
 
 router.get('/google', passport.authenticate('google', {scope: config.oauth.scope}));
 router.get(
-  '/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect('http://localhost:4200/home');
-  }
+  '/google/callback', passport.authenticate('google', { 
+    successRedirect: config.routes.landingUrl,
+    failureRedirect: config.routes.loginUrl, 
+  })
 );
 
-/*
-router.get("/logout", (req, res) => {
-  req.session = null;
-  req.logout();
-  res.redirect('/');
+router.get("/session", (req, res) => {
+  let userCopy: ISafeUser = Object.assign({}, req.user as IUser);
+
+  // Remove sensitive data
+  delete userCopy.ssoID;
+
+  res.send({ user: req.user, isAuthenticated: req.isAuthenticated() });
+});
+
+router.delete("/session", (req, res, next) => {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+  });
+
+  res.send({ success: true });
 })
-*/
 
 export default router;

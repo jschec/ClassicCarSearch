93bda0cf6315013d63bb1412c5b8b1123cb2e02a
jschec <scheck.joshua@gmail.com>
2023-06-03 22:36:48 -0700

    Updated azure-prepper.sh logic

--- azure-prepper.sh
+++ azure-prepper.sh
@@ -2,21 +2,6 @@
 
 SRC_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo "In the following directory:"
-pwd
+rm -v /home/site/wwwroot/*
 
-echo "SCRIPTS_DIR: $SCRIPTS_DIR"
-echo "SRC_DIR: $SRC_DIR"
-
-# Remove unnecessary files
-rm -rf $SRC_DIR/data_provider
-rm -rf $SRC_DIR/front_end
-rm -rf $SRC_DIR/hifi_prototype
-rm -rf $SRC_DIR/scripts
-
-
-# Copy build files to root
-cp -r $SRC_DIR/rest_api/build/* $SRC_DIR/
-
-# Remove build files
-rm -rf $SRC_DIR/rest_api
+cp -r $SRC_DIR/rest_api/build/* /home/site/wwwroot/

eae3eb6c7d4d42875bf0056ff1100080e5179e94
jschec <scheck.joshua@gmail.com>
2023-06-03 22:54:28 -0700

    Reverted azure prepper

--- azure-prepper.sh
+++ azure-prepper.sh
@@ -2,6 +2,19 @@
 
 SRC_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-rm -v /home/site/wwwroot/*
+echo "In the following directory:"
+pwd
 
-cp -r $SRC_DIR/rest_api/build/* /home/site/wwwroot/
+echo "SCRIPTS_DIR: $SCRIPTS_DIR"
+echo "SRC_DIR: $SRC_DIR"
+
+# Remove unnecessary files
+rm -rf $SRC_DIR/data_provider
+rm -rf $SRC_DIR/front_end
+rm -rf $SRC_DIR/hifi_prototype
+rm -rf $SRC_DIR/scripts
+
+# Copy build files to root
+cp -r $SRC_DIR/rest_api/build/* $SRC_DIR/
+
+rm -rf $SRC_DIR/rest_api

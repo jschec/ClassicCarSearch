a3c17669302742721fd43b819b8e10b9fecc7ba1
jschec <scheck.joshua@gmail.com>
2023-06-03 22:26:58 -0700

    Updated azure-prepper.sh

--- azure-prepper.sh
+++ azure-prepper.sh
@@ -3,6 +3,12 @@
 SCRIPTS_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 SRC_DIR=$(dirname "$SCRIPTS_DIR")
 
+echo "In the following directory:"
+pwd
+
+echo "SCRIPTS_DIR: $SCRIPTS_DIR"
+echo "SRC_DIR: $SRC_DIR"
+
 # Remove unnecessary files
 rm -rf $SRC_DIR/data_provider
 rm -rf $SRC_DIR/front_end

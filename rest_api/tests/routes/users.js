3332debbe6463fc9bec945f8d8904ebb8b0baa2a
Zhihai Deng <dzhsurf@gmail.com>
2023-06-05 17:43:41 -0700

    Fix post request

--- rest_api/tests/routes/users.js
+++ rest_api/tests/routes/users.js
@@ -7,58 +7,60 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Test post a new user', function() {
-	var requestResult;
-	var response;
+describe('Test post a new user', function () {
+  var requestResult;
+  var response;
+  var getRequestResult;
 
   before(function (done) {
-      chai.request(`https://${config.hostName}:${config.hostPort}`)
-        .post("/api/users")
-        .send({
-          "ssoID": "jkjghdfgj",
-          "firstName": "Andyr",
-          "lastName": "Rodriguez",
-          "pictureUri": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
-          "email": "Jrodriguez66@gmail.com",
-          "age": 16 })
-        .end(function (err, res) {
-          requestResult = res.body;
-          response = res;
-          expect(err).to.be.null;
-          expect(res).to.have.status(201);
-          done();
-        });
-  });
-  it('Shoulda body', function () {
-		expect(response).to.have.status(201);
-    expect(response.body_id).to.be.an('string');
-    chai.request(`https://${config.hostName}:${config.hostPort}`)
-    .get("/api/users/"+response.body._id)
-    .end(function (err, res) {
-      getrequestResult = res.body;
-      expect(err).to.be.null;
-      expect(res).to.have.status(200);
-      done();
+    var payload = JSON.stringify({
+      "ssoID": "uniquessoID:xxxfff123456",
+      "firstName": "Andyr",
+      "lastName": "Rodriguez",
+      "pictureUri": "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+      "email": "Jrodriguez66-test@gmail.com",
+      "age": 16
     });
+    chai.request(`https://${config.hostName}:${config.hostPort}`)
+      .post("/api/users")
+      .set('Content-Type', 'application/json')
+      .send(payload)
+      .end(function (err, res) {
+        requestResult = res.body;
+        response = res;
+        expect(err).to.be.null;
+        expect(res).to.have.status(201);
+        done();
+      });
+  });
+
+  it('Should return a user', function (done) {
+    expect(response).to.have.status(201);
+    expect(response.body.id).to.be.an('string');
+    chai.request(`https://${config.hostName}:${config.hostPort}`)
+      .get("/api/users/" + response.body.id)
+      .end(function (err, res) {
+        getRequestResult = res.body;
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        done();
+      });
+
     it('compare post request body and get body', function () {
-      expect(getrequestResult).to.include(requestResult);
+      expect(getRequestResult).to.include(requestResult);
     });
 
-        
   });
 
   after(function (done) {
     //clean up
     chai.request(`https://${config.hostName}:${config.hostPort}`)
-        .delete("/api/users/"+response.body._id)
-        .end(function (err, res) {
-          expect(err).to.be.null;
-          expect(res).to.have.status(204);
-          done();
-        
-      }); 
-    }); 
-
-
+      .delete("/api/users/" + response.body.id)
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(204);
+        done();
+      });
+  });
 
 });
